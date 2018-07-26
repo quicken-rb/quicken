4f169972c76e23bec216276359978f5b2bc28017
Matteo Joliveau <matteo.joliveau@mikamai.com>
2018-07-26 17:39:35 +0200

    Version command

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source "https://rubygems.org"
 
+ruby '2.5.1'
+
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-# Specify your gem's dependencies in qk.gemspec
+# Specify your gem's dependencies in quicken.gemspec
 gemspec
+
--- lib/quicken.rb
+++ lib/quicken.rb
@@ -1,5 +1,22 @@
-require "quicken/version"
+# frozen_string_literal: true
+
+require 'thor'
+require 'quicken/version'
 
 module Quicken
-  # Your code goes here...
+  class App < Thor
+    option :version, type: :boolean, aliases: [:v]
+    desc 'qk', 'Quicken base command'
+
+    def default
+      if options.version?
+        puts "Quicken #{Quicken::VERSION}"
+      else
+        help
+      end
+
+    end
+
+    default_command :default
+  end
 end
